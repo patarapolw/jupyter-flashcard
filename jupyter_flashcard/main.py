b8679ea67c16efe93d9eb9e91ef62ac609b346c1
patarapolw <patarapolw@gmail.com>
2018-09-29 11:59:57 +0700

    Add docstring

--- jupyter_flashcard/main.py
+++ jupyter_flashcard/main.py
@@ -10,6 +10,27 @@ from .config import config
 
 class JupyterFlashcard:
     def __init__(self, **kwargs):
+        """Supported kwargs are the same as in config dict
+        {
+            'engine': 'postgresql://localhost/jupyter-flashcard',
+            'host': 'localhost',
+            'port': 7000,
+            'debug': False,
+            'threaded': False,
+            'srs': {
+                1: timedelta(minutes=10),
+                2: timedelta(hours=4),
+                3: timedelta(hours=8),
+                4: timedelta(days=1),
+                5: timedelta(days=3),
+                6: timedelta(days=7),
+                7: timedelta(weeks=2),
+                8: timedelta(weeks=4),
+                9: timedelta(weeks=16)
+            }
+        }
+        """
+
         config.update(kwargs)
 
         self.engine = create_engine(config['engine'])
@@ -18,27 +39,69 @@ class JupyterFlashcard:
         config['session'] = self.session
 
     def __iter__(self):
+        """Default iterator is the same as iterating through files
+        
+        Returns:
+            iterator -- self.files iterator
+        """
+
         return iter(self.files)
 
     @property
     def files(self):
+        """Query object of files
+        
+        Returns:
+            SQLAlchemy Query object -- All db.File's
+        """
+
         return self.session.query(db.File)
 
     @property
     def flashcards(self):
+        """Query object of flashcards
+        
+        Returns:
+            SQLAlchemy Query object -- All db.Flashcard's
+        """
+
         return self.session.query(db.Flashcard)
 
     @property
     def cells(self):
+        """Query object of cells
+        
+        Returns:
+            SQLAlchemy Query object -- All db.Cell's
+        """
+
         return self.session.query(db.Cell)
 
     def init(self, initial_file_path=None):
+        """Initiate the JupyterFlashcard database for the first time
+        
+        Keyword Arguments:
+            initial_file_path {str, pathlib.Path} -- 
+                The initial file path to scan for Jupyter Notebook files.
+                If None, no initial file path is scanned. 
+                (default: {None})
+        """
+
         db.Base.metadata.create_all(self.engine)
 
         if initial_file_path:
             self.add(initial_file_path)
 
     def search_files(self, filename=None, tags=None):
+        """Searching through files in the database
+        
+        Keyword Arguments:
+            filename {str} -- Substring of filenames (default: {None})
+            tags {iterable} -- Iterable of substrings of tags (default: {None})
+        Yields:
+            db.File SQLAlchemy object -- The object matching the criteria
+        """
+
         for db_file in self.files:
             if filename:
                 if filename not in db_file.name:
@@ -60,18 +123,22 @@ class JupyterFlashcard:
                           due=None,
                           tags=None,
                           filename=None):
+        """Searching through flashcards in the database
+        
+        Keyword Arguments:
+            content {str} -- Substring matching db.Flashcard.data (default: {None})
+            min_srs {int} -- Minimal db.Flashcard.srs_level (default: {0})
+            max_srs {int} -- Maximal db.Flashcard.srs_level (default: {None})
+            due {datetime.datetime, datetime.timedelta} -- 
+                Minimal due datetime, or timedelta distance from due datetime
+                For example, datetime.now() or timedelta(hours=0)
+                (default: {None})
+            tags {iterable} -- Iterable of substrings of tags (default: {None})
+            filename {str} -- Substring of filename (default: {None})
+        Yields:
+            db.Flashcard SQLAlchemy object -- The object matching the criteria
         """
 
-        :return:
-        keywords to query: {
-            'cells': self.cells,
-            'srs_level': self.srs_level,
-            'next_review': self.next_review,
-            'modified': self.modified,
-            'tags': self.tags,
-            'filenames': self.filenames
-        }
-        """
         def _has_content(_db_flashcard):
             for _db_cell in _db_flashcard.cells:
                 if content in _db_cell.data:
@@ -132,15 +199,17 @@ class JupyterFlashcard:
                      content=None,
                      filename=None,
                      tags=None):
+        """Searching through cells in the database
+        
+        Keyword Arguments:
+            content {str} -- Substring matching db.Cell.data (default: {None})
+            filename {str} -- Substring of filename (default: {None})
+            tags {iterable} -- Iterable of substring of tags (default: {None})
+        
+        Yields:
+            db.Cell SQLAlchemy object -- The object matching the criteria
         """
 
-        :return:
-        keywords to query: {
-            'data': self.data,
-            'filename': self.filename,
-            'tags': self.tags
-        }
-        """
 
         def _has_tag(_db_cell, _tag):
             for t in _db_cell.tags:
@@ -172,18 +241,52 @@ class JupyterFlashcard:
             yield db_cell
 
     def iter_quiz(self, tags=None):
+        """Generate an iterator of db.Flashcard quiz
+        
+        Keyword Arguments:
+            tags {iterable} -- Iterable of substring of tags (default: {None})
+        
+        Returns:
+            iterator -- 
+                Iterator of db.Flashcard
+                To use it, create a iterator, and then call next() repeatedly (see README.md)
+        """
+
         quiz_list = list(self.search_flashcards(due=datetime.now(), tags=tags))
         random.shuffle(quiz_list)
 
         return iter(quiz_list)
 
     def quiz(self, *args, **kwargs):
+        """Quiz one db.Flashcard of the quiz
+        
+        Returns:
+            db.Flashcard SQLAlchemy object -- A randomized db.Flashcard matching the criteria
+        """
+
         return next(self.iter_quiz(*args, **kwargs))
 
     @classmethod
     def add(cls, fp):
+        """Add a Jupyter Notebook file to the database
+        
+        Arguments:
+            fp {str|Path} -- A Jupyter Notebook file or a folder containing Jupyter Notebook files.
+        """
+
         db.File.add(fp)
 
-    def update(self):
-        for db_file in self.files:
+    def update(self, *args, **kwargs):
+        """Update all files in the database
+
+        Arguments:
+            Same as self.search_files()
+        """
+
+        if args or kwargs:
+            files = self.search_files(*args, **kwargs)
+        else:
+            files = self.files
+
+        for db_file in files:
             db_file.update()
